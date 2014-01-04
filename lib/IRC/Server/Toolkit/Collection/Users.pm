414e0b5356825d0f14a829b18ba5989edb1916e1
Jon Portnoy <avenj@cobaltirc.org>
2014-01-04 15:47:04 -0500

    reasonably complete Collection::Users
    	modified:   lib/IRC/Server/Toolkit/Collection/Users.pm

--- lib/IRC/Server/Toolkit/Collection/Users.pm
+++ lib/IRC/Server/Toolkit/Collection/Users.pm
@@ -1,6 +1,9 @@
 package IRC::Server::Toolkit::Collection::Users;
 use Defaults::Modern;
 
+use IRC::Toolkit::Masks;
+
+use Module::Runtime 'use_module';
 
 use Moo; use MooX::late;
 
@@ -8,7 +11,7 @@ has casemap => (
   required => 1,
   is       => 'ro',
   isa      => sub {
-    # FIXME validate casemap
+    # FIXME validate casemap (IRC::Toolkit::Types?)
   },
 );
 
@@ -21,16 +24,41 @@ has _users => (
   is      => 'ro',
   isa     => HashObj,
   coerce  => 1,
-  default => sub { hash },
+  builder => sub { hash },
 );
 
+has user_class => (
+  lazy    => 1,
+  is      => 'ro',
+  isa     => Str,
+  builder => sub { use_module('IRC::Server::Toolkit::User') },
+);
+
+method create_and_add (@params) {
+  $self->add_users( $self->user_class->new(@params) )
+}
+
 method add_users (@users) {
-  # FIXME
+  my $user_class = $self->user_class;
+  for my $user (@users) {
+    confess "Expected an instance of $user_class but got $user"
+      unless blessed $user and $user->isa($user_class);
+    my $nick = $self->lower( $user->nick );
+    confess "Attempted to add previously extant nick $nick"
+      if $self->_users->exists($nick);
+    $self->_users->set($nick => $user)
+  }
+  $self
 }
 { no warnings 'once'; *add_user = *add_users }
 
 method del_users (@users) {
-  # FIXME take strs or objs
+  my @deleted;
+  for my $user (@users) {
+    my $nick = blessed $user ? $self->lower( $user->nick ) : $user;
+    push @deleted, $nick if $self->_users->delete($nick)
+  }
+  @deleted
 }
 { no warnings 'once'; *del_user = *del_users }
 
@@ -42,21 +70,63 @@ method nickname_array { $self->_users->values->map(sub { $_->nick }) }
 method by_name (Str $nick) { $self->_users->get( $self->lower($nick) ) }
 { no warnings 'once'; *by_nick = *by_name }
 
-method nick_matching (Str $mask) {
-  # FIXME
+method matching_nick (Str $mask) {
+  my $cmap = $self->casemap;
+  my $matches = array;
+  for ($self->_users->kv->all) {
+    my ($nick, $obj) = @$_;
+    $matches->push($obj) if matches_mask( $mask, $nick, $cmap )
+  }
+  $matches->has_any ? $matches : ()
 }
 
-method host_matching (Str $mask) {
-  # FIXME
+method matching_host (Str $mask) {
+  my $cmap = $self->casemap;
+  my $matches = array;
+  $self->_users->values->visit(
+    sub { $matches->push($_) if matches_mask( $mask, $_->host, $cmap ) }
+  )
+  $matches->has_any ? $matches : ()
 }
 
-method user_matching (Str $mask) {
-  # FIXME
+method matching_user (Str $mask) {
+  my $cmap = $self->casemap;
+  my $matches = array;
+  $self->_users->values->visit(
+    sub { $matches->push($_) if matches_mask( $mask, $_->user, $cmap ) }
+  )
+  $matches->has_any ? $matches : ()
 }
 
-method full_matching (Str $mask) {
-  # FIXME
+method matching_full (Str $mask) {
+  my $cmap = $self->casemap;
+  my $matches = array;
+  $self->_users->values->visit(
+    sub { $matches->push($_) if matches_mask( $mask, $_->full, $cmap ) }
+  )
+  $matches->has_any ? $matches : ()
 }
 
 
 1;
+
+=pod
+
+=head1 NAME
+
+IRC::Server::Toolkit::Collection::Users - A set of User objects
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head2 Attributes
+
+=head2 Methods
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+
+=cut
