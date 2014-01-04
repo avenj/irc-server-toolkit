8168a2744c73595efb378c94a38f5ca2b9dfc1c0
Jon Portnoy <avenj@cobaltirc.org>
2014-01-04 18:11:23 -0500

    User POD, cleanups
    	modified:   lib/IRC/Server/Toolkit/User.pm

--- lib/IRC/Server/Toolkit/User.pm
+++ lib/IRC/Server/Toolkit/User.pm
@@ -1,5 +1,8 @@
 package IRC::Server::Toolkit::User;
-use Defaults::Modern;
+use Defaults::Modern
+  -with_types => [
+    'IRC::Server::Toolkit::Types'
+  ];
 
 
 use Moo; use MooX::late;
@@ -40,7 +43,10 @@ has full => (
   isa       => Str,
   writer    => '_set_full',
   predicate => '_has_full',
-  builder   => method { $self->nick .'!'. $self->user .'@'. $self->host },
+  builder   => sub { 
+    my ($self) = @_;
+    $self->nick .'!'. $self->user .'@'. $self->host 
+  },
 );
 
 method _reset_full {
@@ -61,6 +67,14 @@ has realname => (
   builder  => sub { '' },
 );
 
+has server => (
+  lazy     => 1,
+  is       => 'ro',
+  isa      => Str,
+  writer   => 'set_server',
+  builder  => sub { '' },
+);
+
 
 has _chans => (
   is       => 'ro',
@@ -70,12 +84,23 @@ has _chans => (
   builder  => sub { hash },
 );
 
-method add_channel ($channel) { $self->_chans->set($channel => 1) }
-method del_channel ($channel) { $self->_chans->delete($channel) }
-method list_channels { $self->_chans->keys->all }
+method add_channel (
+  ChannelObject $channel
+) {
+  $self->_chans->set($channel => $channel);
+  Scalar::Util::weaken($self->_chans->{$channel});
+  $self
+}
+
+method del_channel ($channel) { 
+  $self->_chans->delete($channel)->has_any
+}
+
+method list_channel_names { $self->_chans->keys->all }
+method list_channel_objects { $self->_chans->values->all }
 
 
-has _modes => (
+has modes => (
   lazy    => 1,
   is      => 'ro',
   isa     => HashObj,
@@ -85,7 +110,7 @@ has _modes => (
 );
 
 method set_mode (
-  InstanceOf['IRC::Mode::Set'] $modes
+  ModeSet $modes
 ) {
   # FIXME still needs some cute mode validation bits
   # and a way to feed IRC::Mode::Set from Collection::Users perhaps
@@ -93,23 +118,24 @@ method set_mode (
   MSET: while (my $mset = $modes->next) {
     my ($flag, $mode, $param) = @$mset;
     if ($flag eq '+') {
-      $param //= 1;
-      my $extant = $self->_modes->get($mode);
+      $param //= -1;
+      my $extant = $self->modes->get($mode);
       next MSET if defined $extant and $extant eq $param;
-      $self->_modes->set($mode => $param);
+      $self->modes->set($mode => $param);
       push @changed, [ $flag, $mode, $param ]
     } elsif ($flag eq '-') {
-      my $extant = $self->_modes->get($mode);
+      my $extant = $self->modes->get($mode);
       if (defined $extant) {
         push @changed, [ $flag, $mode, $extant ];
-        $self->_modes->delete($mode)
+        $self->modes->delete($mode)
       }
     } else {
       confess "Unknown mode flag '$flag'"
     }
   }
   $modes->reset;
-  array(@changed)
+  state $guard = do { require IRC::Mode::Set };
+  IRC::Mode::Set->new(mode_array => \@changed)
 }
 
 
@@ -131,6 +157,137 @@ method has_flags (@flags) {
   @res
 }
 
+
+=pod
+
+=for Pod::Coverage set_\w+
+
+=head1 NAME
+
+IRC::Server::Toolkit::User - Base class for connected IRC users
+
+=head1 SYNOPSIS
+
+FIXME
+
+=head1 DESCRIPTION
+
+A base class for objects representing an IRC user.
+
+These objects stringify to the value of the L</nick> attribute.
+
+Also see L<IRC::Server::Toolkit::Collection::Users>.
+
+=head2 Attributes
+
+=head3 nick
+
+Required.
+
+The user's nickname.
+
+Can be set via B<set_nick>
+
+=head3 user
+
+The user's ident ('username') string.
+
+Can be set via B<set_user>
+
+=head3 host
+
+The user's host address.
+
+Can be set via B<set_host>
+
+=head3 full
+
+The user's full 'nick!user@host' IRC identifier.
+
+Also see L</nick>, L</user>, L</host>
+
+=head3 realname
+
+The user's GECOS ('realname') string.
+
+Can be set via B<set_realname>
+
+=head3 server
+
+The server name the user belongs to.
+
+Can be set via B<set_server>
+
+=head3 modes
+
+  if ( $user->modes->exists('w') ) {
+    # ...
+  }
+
+  my $snomask = $user->modes->get('s');
+
+A L<List::Objects::WithUtils::Hash> mapping mode characters to their params
+(or -1 for paramless modes).
+
+=head2 Methods
+
+=head3 Channels
+
+=head4 add_channel
+
+Takes an L<IRC::Server::Toolkit::Channel> and adds a weak reference to the
+channel to the user's channel list.
+
+See L</del_channel>, L</list_channels>.
+
+=head4 del_channel
+
+Takes the name of a channel or an L<IRC::Server::Toolkit::Channel> instance.
+Returns boolean true if the channel was deleted.
+
+=head4 list_channel_names
+
+Returns the names of the channels the user is present on as a plain list.
+
+=head4 list_channel_objects
+
+Returns the objects representing the channels the user is present on as a
+plain list.
+
+=head3 Modes
+
+=head4 set_mode
+
+Takes an L<IRC::Mode::Set> containing user mode changes and adjusts L</modes>
+accordingly.
+
+Returns a new L<IRC::Mode::Set> containing the modes that were actually changed.
+
+=head3 Flags
+
+=head4 list_flags
+
+Returns all currently enabled flags (as a plain list).
+
+=head4 add_flags
+
+Enables the given list of flags (which can be arbitrary strings).
+
+=head4 del_flags
+
+Removes the given list of flags.
+
+=head4 has_flags
+
+Takes a list of flags and returns a new list containing the flags that are
+currently enabled.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+
+=cut
 
 
 1;
