befaca7b5c438a845b975a8bc1682432edfa10b9
Jon Portnoy <avenj@cobaltirc.org>
2014-01-04 19:31:47 -0500

    Types expansion
    	modified:   lib/IRC/Server/Toolkit/Types.pm

--- lib/IRC/Server/Toolkit/Types.pm
+++ lib/IRC/Server/Toolkit/Types.pm
@@ -1,10 +1,16 @@
 package IRC::Server::Toolkit::Types;
 use strict; use warnings FATAL => 'all';
+use v5.14;
 
 use Type::Library     -base;
 use Type::Utils       -all;
 use Types::Standard   -types;
 use Types::TypeTiny   ();
+
+
+use IRC::Mode::Set;
+use IRC::Mode::Single;
+
 
 declare ValidCaseMap =>
   as Str(),
@@ -22,8 +28,12 @@ declare ValidCaseMap =>
 declare ChannelObject =>
   as InstanceOf['IRC::Server::Toolkit::Channel'];
 
+declare StateObject =>
+  as InstanceOf['IRC::Server::Toolkit::State'];
+
 declare UserObject =>
   as InstanceOf['IRC::Server::Toolkit::User'];
+
   
 declare ChannelCollection =>
   as InstanceOf['IRC::Server::Toolkit::Collection::Channels'];
@@ -34,5 +44,17 @@ declare UserCollection =>
 
 declare ModeSet =>
   as InstanceOf['IRC::Mode::Set'];
+
+coerce ModeSet =>
+  from ArrayRef() => via { IRC::Mode::Set->new(mode_array => $_) },
+  from ArrayObj() => via { IRC::Mode::Set->new(mode_array => $_->unbless) };
+
+
+declare ModeSingle =>
+  as InstanceOf['IRC::Mode::Single'];
+
+coerce ModeSingle =>
+  from ArrayRef() => via { IRC::Mode::Single->new(@$_) },
+  from ArrayObj() => via { IRC::Mode::Single->new($_->all) };
 
 1;
