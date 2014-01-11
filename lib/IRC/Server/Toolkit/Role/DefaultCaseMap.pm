2891b0c167a7d8529184eb0f9d39a3a223db79f4
Jon Portnoy <avenj@cobaltirc.org>
2014-01-11 16:43:15 -0500

    DefaultCaseMap can set_casemap, but clear if eq rfc1459
    	modified:   lib/IRC/Server/Toolkit/Role/DefaultCaseMap.pm

--- lib/IRC/Server/Toolkit/Role/DefaultCaseMap.pm
+++ lib/IRC/Server/Toolkit/Role/DefaultCaseMap.pm
@@ -13,9 +13,20 @@ has _casemap => (
   is        => 'ro',
   isa       => ValidCaseMapObject,
   predicate => 'has_casemap',
+  writer    => 'set_casemap',
+  clearer   => 'clear_casemap',
   coerce    => 1,
-  builder   => sub { 'rfc1459' },
+  builder   => sub { 'rfc1459' },   # shouldn't be called, but eh
 );
+
+around set_casemap => sub {
+  my ($orig, $self, $cmap) = @_;
+  if ($cmap eq 'rfc1459') {
+    $self->clear_casemap
+  } else {
+    $self->$orig($cmap)
+  }
+};
 
 method casemap { $self->has_casemap ? $self->_casemap : 'rfc1459' }
 
